9c76e2a56d614c15a9789624ee048046a61e8a14
SujalMishra <98722988+SujalMishra@users.noreply.github.com>
2023-05-18 09:43:06 +0530

    minor changes done

--- src/components/Card.js
+++ src/components/Card.js
@@ -44,14 +44,14 @@ useEffect(()=>{
   <div className="card-body">
     <h5 className="card-title">{props.foodItem.name}</h5>
     <div className='container w-100'>
-        <select className='m-2 h-100 bg-success rounded' onChange={(e)=>setQty(e.target.value)}>
+        <select className='m-2 h-100 bg-primary rounded' onChange={(e)=>setQty(e.target.value)}>
             {Array.from(Array(6),(e,i)=>{
                 return (
                     <option key ={i+1} value={i+1}>{i+1}</option>
                 )
             })}
         </select>
-        <select className='m-2 h-100  bg-success rounded' ref = {priceRef}onChange={(e)=>setSize(e.target.value)}>
+        <select className='m-2 h-100  bg-primary rounded' ref = {priceRef}onChange={(e)=>setSize(e.target.value)}>
            {priceOptions.map((data)=>{
             return <option key={data} value={data}>{data}</option> 
            })}
@@ -60,7 +60,7 @@ useEffect(()=>{
             ${finalPrice}/-
         </div>
         <hr></hr>
-        <div className='btn btn-success justify-center ms-2' onClick={handleAddToCart}>Add To Cart</div>
+        <div className='btn btn-primary justify-center ms-2' onClick={handleAddToCart}>Add To Cart</div>
     </div>
     
   </div>
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -11,14 +11,14 @@ export default function Navbar() {
   const navigate = useNavigate();
   const handleLogout = () =>{
      localStorage.removeItem('authToken');
-     navigate('/Login');
+     navigate('/');
   }
 
 
 
   return (
     <div>
-     <nav className="navbar navbar-expand-lg navbar-light bg-dark">
+     <nav className="navbar navbar-expand-lg navbar-light bg-primary">
   <div className="container-fluid">
     <Link className="navbar-brand fs-3 fst-italic" to='/' >Foodie</Link>
     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
@@ -29,11 +29,11 @@ export default function Navbar() {
         <li className="nav-item">
           <Link className="nav-link active fs-5" aria-current="page" to="/">Home</Link>
         </li>
-      {(localStorage.getItem("authToken")) ?
+      {/* {(localStorage.getItem("authToken")) ?
          <li className="nav-item">
          <Link className="nav-link active fs-5" aria-current="page" to="/">MyOrder</Link>
        </li> :""
-      }
+      } */}
       </ul>
       { !(localStorage.getItem("authToken"))?
       <div className='d-flex'>
